b271e3dfdbd9d134f9e41e824606637569850985
Max <66183307+maxverwiebe@users.noreply.github.com>
2025-05-05 12:23:00 +0200

    Use SpinnerStyle.DOTS in example1.py

--- example1.py
+++ example1.py
@@ -1,6 +1,6 @@
 import time
 from termspin import Spinner, SpinnerStyle
 
-with Spinner(text="Thinking", theme=SpinnerStyle.WAVE, color="\033[96m"):
+with Spinner(text="Thinking", theme=SpinnerStyle.DOTS, color="\033[96m"):
         time.sleep(2)
     
